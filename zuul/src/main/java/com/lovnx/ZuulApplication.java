94b6966c2e7bbb8d689d38f4bf3c83bab90b47d3
tungshine <tangxushine@163.com>
2019-04-20 21:51:35 +0800

    spring cloud

--- zuul/src/main/java/com/lovnx/ZuulApplication.java
+++ zuul/src/main/java/com/lovnx/ZuulApplication.java
@@ -1,16 +1,11 @@
 package com.lovnx;
 
+import com.lovnx.filter.ErrorFilter;
+import com.lovnx.filter.ResultFilter;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
-
-import com.lovnx.filter.AccessFilter;
-import com.lovnx.filter.ErrorFilter;
-import com.lovnx.filter.RateLimitFilter;
-import com.lovnx.filter.ResultFilter;
-import com.lovnx.filter.UuidFilter;
-import com.lovnx.filter.ValidateFilter;
 
 @EnableZuulProxy
 @SpringCloudApplication
@@ -20,30 +15,30 @@ public class ZuulApplication {
 		new SpringApplicationBuilder(ZuulApplication.class).web(true).run(args);
 	}
 
-	@Bean
-	public AccessFilter accessFilter() {
-		return new AccessFilter();
-	}
+//	@Bean
+//	public AccessFilter accessFilter() {
+//		return new AccessFilter();
+//	}
 	
-	@Bean
-	public RateLimitFilter rateLimiterFilter() {
-		return new RateLimitFilter();
-	}
+//	@Bean
+//	public RateLimitFilter rateLimiterFilter() {
+//		return new RateLimitFilter();
+//	}
 	
 	@Bean
 	public ResultFilter resultFilter() {
 		return new ResultFilter();
 	}
 	
-	@Bean
-	public UuidFilter uuidFilter() {
-		return new UuidFilter();
-	}
+//	@Bean
+//	public UuidFilter uuidFilter() {
+//		return new UuidFilter();
+//	}
 	
-	@Bean
-	public ValidateFilter validateFilter() {
-		return new ValidateFilter();
-	}
+//	@Bean
+//	public ValidateFilter validateFilter() {
+//		return new ValidateFilter();
+//	}
 	
 	@Bean
 	public ErrorFilter errorFilter() {

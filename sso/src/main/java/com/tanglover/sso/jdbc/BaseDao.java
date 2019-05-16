fc9c3daa969a246a40393bd6bc52d4214419f43e
tungshine <362615233@qq.com>
2019-05-16 10:55:27 +0800

    sso

--- sso/src/main/java/com/tanglover/sso/jdbc/BaseDao.java
+++ sso/src/main/java/com/tanglover/sso/jdbc/BaseDao.java
@@ -8,7 +8,9 @@ import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import javax.annotation.Resource;
+import java.sql.SQLException;
 import java.text.SimpleDateFormat;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -29,7 +31,7 @@ public class BaseDao {
     SimpleDateFormat sdfYm = new SimpleDateFormat("yyyyMM");
     SimpleDateFormat sdfYmd = new SimpleDateFormat("yyyyMMdd");
 
-    public NamedParameterJdbcTemplate jdbcTemplate() {
+    public NamedParameterJdbcTemplate getJdbc() {
         return _np;
     }
 
@@ -88,4 +90,76 @@ public class BaseDao {
         return null;
     }
 
+    /**
+     * Execute a query given static SQL, not
+     *
+     * @param keyName
+     * @param keyValue
+     * @param selectByOther_SQL
+     * @return
+     */
+    public List<Map<String, Object>> selectByOther(Object keyName, Object keyValue, String selectByOther_SQL) {
+        try {
+            selectByOther_SQL += String.format("%s = :%s", keyName, keyName);
+            Map paramMap = new HashMap();
+            paramMap.put(keyName, keyValue);
+            return this._np.queryForList(selectByOther_SQL, paramMap);
+        } catch (Exception e) {
+            e.printStackTrace();
+            logger.error("BaseDao selectByOther", e);
+        }
+        return null;
+    }
+
+    /**
+     * Update columns value for one by one
+     *
+     * @param sql
+     * @param paramMap
+     * @return
+     */
+    public int updateBySQL(String sql, Map<String, Object> paramMap) {
+        try {
+            //String sql ="UPDATE user SET password = :password WHERE account = :account";
+            return this._np.update(sql, paramMap);
+        } catch (Exception e) {
+            e.printStackTrace();
+            logger.error("BaseDao updateBySQL", e);
+        }
+        return -1;
+    }
+
+    /**
+     * Query list by sql
+     *
+     * @param sql
+     * @param beanClass
+     * @param args
+     * @param <T>
+     * @return
+     */
+    public <T> List<T> executeQuery(String sql, Class<T> beanClass, Object... args) {
+        try {
+            return this._np.getJdbcOperations().query(sql, new BeanPropertyRowMapper<>(beanClass), args);
+        } catch (Exception e) {
+            e.printStackTrace();
+            logger.error("BaseDao executeQuery", e);
+        }
+        return null;
+    }
+
+    /**
+     * Query unique Object by sql
+     *
+     * @param sql
+     * @param beanClass
+     * @param args
+     * @param <T>
+     * @return
+     * @throws SQLException
+     */
+    public <T> T queryUniqueT(String sql, Class<T> beanClass, Object... args) throws SQLException {
+        List<T> list = executeQuery(sql, beanClass, args);
+        return (null == list || 0 == list.size()) ? null : list.get(0);
+    }
 }

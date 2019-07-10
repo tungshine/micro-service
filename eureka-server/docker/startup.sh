8ca8a02a978185ce2edcef44a0c907761b4856bf
tungshine <362615233@qq.com>
2019-07-10 17:40:28 +0800

    eureka dockerfile

--- eureka-server/docker/startup.sh
+++ eureka-server/docker/startup.sh
@@ -28,5 +28,5 @@ cd /home/tx_workspace/project_git/micro-service/eureka-server &&
 mvn install &&
 cd target &&
 cp eureka-server-0.0.1-SNAPSHOT.jar ../docker &&
-#cd /home/tx_workspace/project_git/micro-service/eureka-server/docker &&
+cd /home/tx_workspace/project_git/micro-service/eureka-server/docker &&
 docker build -t alex/eureka-server:eureka-server_v1.0 .

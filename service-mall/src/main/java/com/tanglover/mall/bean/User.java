3f1d3fb681394989d0bd5bf93e06d544cc7d9dd2
tungshine <tangxushine@163.com>
2020-04-05 14:04:01 +0800

    flower

--- service-mall/src/main/java/com/tanglover/mall/bean/User.java
+++ service-mall/src/main/java/com/tanglover/mall/bean/User.java
@@ -1,10 +1,22 @@
 package com.tanglover.mall.bean;
 
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  * @author TangXu
  * @create 2019-07-16 15:05
  * @description:
  */
+@Getter
+@Setter
 public class User {
+
+    private long id;
+    private String username;
+    private String password;
+    private String birthday;
+    private long registerTime;
+    private long firstLoginTime;
 
 }
